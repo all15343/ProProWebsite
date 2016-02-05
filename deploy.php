0b8a6b50c9069ca605127579198f3149c56224ad
Alexander <all15343@spengergasse.at>
2016-02-05 11:11:52 +0100

    Revert "Enhanced Deploy.php"
    This reverts commit 4feae93d72e5796ead5113af9bbe67f96c8415a1.

--- deploy.php
+++ deploy.php
@@ -1,16 +1,33 @@
 <?php
+	/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket, more deets here:
+	 *
+	 *		https://gist.github.com/1809044
+	 */
+	// The commands
 	$commands = array(
-		'git fetch',
+		'echo $PWD',
+		'whoami',
+  		'git fetch',
 		'git pull',
 		'git status',
-		'git log --graph --abbrev-commit --date=relative --format=format:\'%h - (%ar) %s - %an%d\' --all',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+    		'git log --graph --abbrev-commit --date=relative --format=format:\'%h - (%ar) %s - %an%d\' --all',
 	);
+	// Run the commands for output
 	$output = '';
 	foreach($commands AS $command){
+		// Run it
 		$tmp = shell_exec($command);
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #FF9800;\">{$command}\n</span>";
+		// Output
+		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}
+	// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
@@ -18,10 +35,14 @@
 	<meta charset="UTF-8">
 	<title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #fafafa; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
-	<span style="font-size:40px; color:#FF9800;">ProPro - Deploy Service
-	</span>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+
 <?php echo $output; ?>
 </pre>
 </body>
