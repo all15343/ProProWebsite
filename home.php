708bae03f3e22aea59ee3648535791334921e974
Alexander <all15343@spengergasse.at>
2016-02-05 10:59:45 +0100

    Test

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
     <ad-card header="ARTE SCHWARZ 6 ARM Design Kronleuchter" provider="roomproducts gmbh" imgurl="http://ecx.images-amazon.com/images/I/51NgtPwhumL.jpg" favorite
              href="http://www.amazon.de/ARTE-SCHWARZ-Design-Kronleuchter-roomproducts/dp/B0045TUHP0/ref=sr_1_9?ie=UTF8&qid=1446395972&sr=8-9&keywords=Luster"></ad-card>
 </div>
-<h2 class="like-h1">Küezlich Gekauft:</h2>
+<h2 class="like-h1">Kürzlich Gekauft:</h2>
 
 <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</h3>
 <h4>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</h4>
